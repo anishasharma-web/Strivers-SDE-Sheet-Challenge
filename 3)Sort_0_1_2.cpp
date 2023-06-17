ca7f0bfa0fca7ade7dfa136d94c54476692ce828
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2023-06-17 21:11:14 +0530

    Create 3)Sort_0_1_2.cpp

---
+++ 3)Sort_0_1_2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   /*
+    DUTCH NATIONAL FLAG ALGO 
+    If arr[mid] == 0, swap arr[low] and arr[mid] ,low++and mid++.
+    If arr[mid] == 1, mid++
+    If arr[mid] == 2, swap arr[mid] and arr[high],high--
+   */
+   int low = 0, mid = 0, high = n - 1; 
+
+    while (mid <= high) {
+        if (arr[mid] == 0) {
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+
+}
