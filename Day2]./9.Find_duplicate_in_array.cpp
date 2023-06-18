153775c2253bd0ff3c6261c69bdf9dc6c2a2f43b
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2023-06-18 20:48:22 +0530

    Create 9.Find_duplicate_in_array.cpp

---
+++ Day2]./9.Find_duplicate_in_array.cpp
@@ -0,0 +1,21 @@
+int ml=m-1,nl=n-1;
+        int t=m+n-1;
+        for(int i=m+n-1; i> 0  and ml>=0 and nl>=0;i--)
+        {
+            if(nums1[ml]>=nums2[nl])
+            {
+                nums1[t]=nums1[ml];
+                ml--;
+                t--;
+            }
+            else
+            {
+                nums1[t]=nums2[nl];
+                nl--;
+                t--;
+            }
+        }
+        while(nl>=0)
+            {
+                nums1[t--]=nums2[nl--];
+            }
